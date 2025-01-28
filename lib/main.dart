319d9e35135a66c0fb23c9bfdc1a926b8ac6e567
Yogita bisht <yogitabisht.4april@gmail.com>
2025-01-28 23:10:32 +0530

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,210 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp(
+    title: 'Flutter Demo',
+    message: 'Flutter is an open-source cross-platform mobile application development SDK created by Google. It is highly user-friendly and builds high-quality mobile applications. The intention behind this article is to guide readers through the process of building an application through flutter by creating a simple Flutter App on Android Studio.',
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+    required this.title,
+    required this.message,
+  });
 
-  // This widget is the root of your application.
+  final String title;
+  final String message;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_alert_outlined),
+              onPressed: () {},
             ),
           ],
         ),
+         body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            
+            children: [
+              const SizedBox(height: 20), 
+          Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+              color: Colors.grey.withValues(alpha: 0.5),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+            ),
+            child:
+            Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    children: [
+                      Text('Flutter', style: TextStyle(fontWeight: FontWeight.w900,color: Colors.blue,fontSize:20),textAlign: TextAlign.center, ),
+
+                      Text('Flutter is an open-source cross-platform mobile application development SDK',textAlign:  TextAlign.center,),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.star, color: Colors.green[500]),
+                          Icon(Icons.star, color: Colors.green[500]),
+                          Icon(Icons.star, color: Colors.green[500]),
+                          Icon(Icons.star, color: Colors.black),
+                          Icon(Icons.star, color: Colors.black),
+                          Text('170 '),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: const Image(
+                    image: AssetImage('assets/images/R.jpeg'),
+                  ),
+                ),
+
+              ],
+            ),
+            // Add more widgets here if needed
+         
+          ),
+            ],
+         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+          );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    super.key,
+    required this.title,
+    required this.message,
+  });
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter ',
+      theme: ThemeData(
+    primarySwatch: Colors.blue
+    ),
+      
+      home: Scaffold(
+        appBar:AppBar(
+          title:Text('FLUTTER APPLICATION'),
+          centerTitle: true,
+          actions: <Widget>[
+            
+          IconButton(
+            icon: const Icon(Icons.add_alert_outlined),
+            onPressed:(){},
+          ),
+  ],
+       
+        ),
+        body:
+         Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(message),
+             
+        
+              const Divider(
+                color: Color.fromARGB(255, 0, 0, 0), 
+                height: 20, 
+                thickness: 2, 
+                indent: 20, 
+                endIndent: 20,
+              ),
+          
+            const Image(image: AssetImage('assets/images/R.jpeg'),),
+
+            const Divider(
+                color: Color.fromARGB(255, 0, 0, 0), 
+                height: 20, 
+                thickness: 2, 
+                indent: 20, 
+                endIndent: 20,
+              ),
+              
+              ElevatedButton(
+                onPressed:(){
+                  Fluttertoast.showToast(
+                      msg: "This is Center Short Toast",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0
+                  );
+
+                },
+                child: const Text('Press Me'),
+              ),
+
+               const Icon(
+              Icons.access_alarms,
+              color: Color.fromARGB(255, 67, 54, 244),
+              size: 50.0,
+
+              
+              
+              ),
+
+            
+            
+             Container(
+            width: 2,
+           height: 100,
+          color: const Color.fromARGB(255, 0, 0, 0),
+),
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
